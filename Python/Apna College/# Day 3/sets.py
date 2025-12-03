64e44d5a01632444c879a5c094b47038a0e10262
Farhan-uxcoder <mohammedfarhan1089@gmail.com>
2025-12-03 21:14:31 +0530

    added the sets methods

--- Python/Apna College/# Day 3/sets.py
+++ Python/Apna College/# Day 3/sets.py
@@ -1,1 +1,11 @@
 #sets are the collection of  unique elements
+#in sets the elements cant be negative
+s = {1,2,3,4,4,4,4,4}
+print(s)
+s.add(12)
+print(s)
+
+s1 = {1,2,3,4,5}
+s2 = {4,5,6,7,8,9}
+print(s1.union(s2))
+print(s1.intersection(s2))
