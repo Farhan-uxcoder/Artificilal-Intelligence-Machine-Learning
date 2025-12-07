647235522dcb0943fedf38eea6948e11a4443c28
Farhan-uxcoder <mohammedfarhan1089@gmail.com>
2025-12-07 09:58:04 +0530

    added the even or odd

--- Python/Apna College/# Day 2/even_or_odd.py
+++ Python/Apna College/# Day 2/even_or_odd.py
@@ -1,8 +1,8 @@
 
-a = int(input("enter the number to check weather its even or odd "))
-if (a%2)==0:
-    print(a," is even")
-else:
+# a = int(input("enter the number to check weather its even or odd "))
+# if (a%2)==0:
+#     print(a," is even")
+# else:
 
 a = int(input("enter the number to check weather its even or odd "))
 if (a%2)==0:
