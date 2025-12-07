e6d148bf00c4d7f6f52917f816bcc94677f649ce
Farhan-uxcoder <mohammedfarhan1089@gmail.com>
2025-12-07 11:20:31 +0530

    added the types of constructors

---
+++ Python/Apna College/# Day 4/types of constructor.py
@@ -0,0 +1,15 @@
+#there are two types of constructors
+#default constructors and parameteriesd constructor
+
+class student:
+    clg_name = "Acharya institute of technology"  #class attributes
+
+    def __init__(self,name,gpa,branch):
+        self.name = name         #method attributes
+        self.branch = branch
+        self.gpa = gpa
+
+
+s1 = student("farhan",9.2,"ISE")
+print(student.clg_name)
+print(f"The student {s1.name} is studying in the {s1.branch} branch with the {s1.gpa} gpa")
