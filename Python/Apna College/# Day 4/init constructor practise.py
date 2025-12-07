06be3a22611c9e75c00fc8a1eac2bfc41dbd65f6
Farhan-uxcoder <mohammedfarhan1089@gmail.com>
2025-12-07 10:38:02 +0530

    added some more practise cases

--- Python/Apna College/# Day 4/init constructor practise.py
+++ Python/Apna College/# Day 4/init constructor practise.py
@@ -14,5 +14,29 @@ class library:
         self.reciver = reciver
         self.submittedDate = submittedDate
 
+    def get_id(self):
+       return self.bookid
+
 b1 = library("java",45,"farhan",12)
+b2 = library("python",35,"farhann",113)
+
 print(b1.bookid,b1.reciver,b1.submittedDate,b1.bookname)
+print(b2.get_id())
+
+
+#for class of students
+class students :
+    def __init__(self,name,hobby,age):
+        self.name = name
+        self.age = age
+        self.hobby = hobby
+
+    def get_name(self):
+         return self.name
+    def get_hobby(self):
+        return self.hobby
+    def get_age(self):
+        return self.age
+
+s1 = students("meow","singing",12)
+print(f"Name is {s1.get_name()}\nAge is {s1.get_age()} ")
