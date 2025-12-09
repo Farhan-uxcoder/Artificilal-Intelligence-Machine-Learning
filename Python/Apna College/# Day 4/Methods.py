b4b719cb38c4148e05cde9d78720dc9e003aed22
Farhan-uxcoder <mohammedfarhan1089@gmail.com>
2025-12-09 17:48:32 +0530

    instance methods added

---
+++ Python/Apna College/# Day 4/Methods.py
@@ -0,0 +1,22 @@
+'''
+
+There are Three types of methods are there i.e
+1. Instace
+2. Class
+3.static
+
+
+'''
+class Laptop:
+    memory_type ="SSD"
+    
+    def __init__(self,memory,RAM):
+        self.memory = memory
+        self.RAM = RAM
+        
+    def get_info(self):
+        print(f"Laptop having the following specs \n Memory of {self.memory} \n RAM of {self.RAM} with the {self.memory_type} ")
+        
+
+l1 = Laptop("256","16")
+l1.get_info()
