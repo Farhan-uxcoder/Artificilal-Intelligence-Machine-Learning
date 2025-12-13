13692f08cc8705fa90c887c0f240b0c4fbd3a616
Farhan-uxcoder <mohammedfarhan1089@gmail.com>
2025-12-13 18:37:24 +0530

    Single level inheritence added

---
+++ Python/Apna College/# Day 4/OOPS/Types_of_inheritence.py
@@ -0,0 +1,19 @@
+'''
+There are 3 types of inheritence
+
+1.Single level inheritence:
+    The simple inheritence where the child class have the properties of a parent
+    eg:
+            |--------------|
+            |    parent    |
+            |--------------|
+                    |
+                    |
+            |--------------|
+            |    child    |
+            |--------------|
+            
+2.Multi level inheritence
+3.Multiple level inheritence
+'''
+
