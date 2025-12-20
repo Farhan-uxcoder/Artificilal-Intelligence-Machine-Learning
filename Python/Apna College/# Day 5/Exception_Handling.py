b604d1d1ff1637ba8ca4692e6a43be8dbb60638b
Farhan-uxcoder <mohammedfarhan1089@gmail.com>
2025-12-20 19:09:40 +0530

    Exception Handling added

---
+++ Python/Apna College/# Day 5/Exception_Handling.py
@@ -0,0 +1,16 @@
+# x = int(input("enter x:"))
+# ans = 10/x
+# print(ans)
+
+#now lets write this using the exception handling
+
+try:
+    x = int(input("Enter x:"))
+    ans = 10/x
+except ZeroDivisionError:
+    print("Division with zero is not possible")
+except ValueError:
+    print("enter a integer value")
+    
+else:
+    print(ans)
