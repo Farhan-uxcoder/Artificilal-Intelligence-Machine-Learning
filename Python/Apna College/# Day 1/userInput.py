ba2759828f52991ed102ba076727f853a008b4e4
Farhan-uxcoder <mohammedfarhan1089@gmail.com>
2026-02-24 15:30:36 +0530

    added unadded files

--- Python/Apna College/# Day 1/userInput.py
+++ Python/Apna College/# Day 1/userInput.py
@@ -1,6 +1,7 @@
 #getting input by using the input() function
 
 x = input("enter value of a ")
+print(x)
 a = int(input("enter value of a "))
 a = float(input("enter value of a "))
 
--- Python/Apna College/# Day 5/Lists_comprehensions.py
+++ Python/Apna College/# Day 5/Lists_comprehensions.py
@@ -1,14 +1,18 @@
+# Using a loop
 sq = []
-for i in range (6):
-    sq.append(i*i)
+for i in range(6):
+    sq.append(i * i)
 print(sq)
 
-squre = [i*i for i in range(6)]
-print(squre)
+# Using list comprehension
+square = [i * i for i in range(6)]
+print(square)
 
-squre = [i*i for i in range(6) if((i%2)!=0)]
-print(squre)
+# Squares of odd numbers only
+square = [i * i for i in range(6) if (i % 2) != 0]
+print(square)
 
+# Lengths of words
 words = ["apple", "banana", "cherry"]
 lengths = [len(word) for word in words]
 print(lengths)
